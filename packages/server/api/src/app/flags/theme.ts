5d73724960eb806c97fe275c9af9533bd8ecef44
MukulBedreErelgo <mukul.bedre@erelego.com>
2024-12-17 15:58:42 +0530

    Update theme.ts

--- packages/server/api/src/app/flags/theme.ts
+++ packages/server/api/src/app/flags/theme.ts
@@ -153,7 +153,7 @@ export function generateTheme({
 
 export const defaultTheme = generateTheme({
     primaryColor: '#6e41e2',
-    websiteName: 'Activepieces',
+    websiteName: 'Munshify Workflow',
     fullLogoUrl: 'https://cdn.munshify.com/logos/munshifyLogo.png',
     favIconUrl: 'https://cdn.munshify.com/logos/munshifyLogo.png',
     logoIconUrl: 'https://cdn.munshify.com/logos/munshifyLogo.png',
